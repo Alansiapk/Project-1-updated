2afc1f8c95b480ef23c3f535a1266b51b17a186c
Alansiapk <alansiapk0315@gmail.com>
2023-02-16 07:55:16 +0000

    added marker

--- script.js
+++ script.js
@@ -4,33 +4,47 @@ const START_POINT = [1.3521,103.8198]
 
 
 async function loadData(lat,lng, query) {
+    
     let response = await axios.get("https://api.foursquare.com/v3/places/search", {
         "headers": {
             "Accept": "application/json",
             "Authorization": FOURSQUARE_API_KEY
         },
         "params": {
-            "ll": lat+",",lng,
+            "ll": lat+","+lng,
             "query": query,
+            // "ll": "1.3521,103.8198",
+            // "query": "restaurant"
         },
 
     });
-    console.log(response.data);
+   
     return response.data;
-}
 
+}
+//loadData();
 
 document.querySelector("#btnSearch").addEventListener("click", async function () {
     let searchValue = document.querySelector('#searchValue').value;
-        let searchResults = await loadData(START_POINT[0], START_POINT[1], searchValue);
-        console.log(searchResults);
-
+    console.log(START_POINT[0]);
+    console.log(START_POINT[1])
+        let searchResults = await loadData(START_POINT[0],START_POINT[1], searchValue);
+        searchResultLayer.clearLayers();
+        console.log(searchResults)
+        // console.log(searchResults);
+        for (let result of searchResults.results) {
+            let coordinate = [result.geocodes.main.latitude, result.geocodes.main.longitude ]
+            let marker = L.marker(coordinate).addTo(searchResultLayer);
+            marker.bindPopup(`<h1>${result.name}<h1>
+                <h2>${result.location.formatted_address}</h2>
+                `);
+        }
     
 });
 
-
-
-let map = L.map("map").setView(START_POINT,13)
+let map = L.map("map").setView(START_POINT,13);
+let searchResultLayer = L.layerGroup();
+searchResultLayer.addTo(map)
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
