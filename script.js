65222991abb2e3d86af4d59ce3d30eca49d5ef3c
Alansiapk <alansiapk0315@gmail.com>
2023-02-17 12:12:26 +0800

    added in markercluster css

--- script.js
+++ script.js
@@ -44,3 +44,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+//part 2
+function getRandomLatLng(map){
+
+    
+}
