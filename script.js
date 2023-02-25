3c8eb2474a2371f44a86ca44c2ef91798f282edb
Alansiapk <alansiapk0315@gmail.com>
2023-02-25 18:00:53 +0800

    added marker cluster and change icon

--- script.js
+++ script.js
@@ -35,11 +35,25 @@ const foursquare = {
 
 }
 
-// create layers for each category
-let MRTLayer = L.layerGroup();
+// create layers for each category change to markercluster group
+//google search = leaftlet marker cluster
+// let restaurantLayer = L.layerGroup()
+// let MRTLayer = L.markerClusterGroup({
+//     iconCreateFunction: function(cluster) {
+//         return L.divIcon({
+//             html: `<div class="customMarkerClusterIcon">
+//             ${cluster.getChildCount()}
+//             </div>`
+//         });
+//     }
+// });
+let MRTLayer = L.markerClusterGroup();
 let restaurantLayer = L.layerGroup();
 let gymLayer = L.layerGroup();
 let barLayer = L.layerGroup();
+
+let searchRadiusLayer = L.layerGroup().addTo(map);
+let searchGymLayer = L.layerGroup().addTo(map);
 
 // add the layers to the map
 // map.addLayer(MRTLayer);
@@ -59,14 +73,43 @@ let overlays = {
 // add overlay to map
 L.control.layers({}, overlays).addTo(map);
 
+//ICON
+const restaurantIcon = L.icon({
+    iconUrl: 'img/restaurant.png',
+    iconSize: [45,45],
+    iconAnchor:[23,45],
+    popupAnchor: [0,0]
+})
+
+const mrtIcon = L.icon({
+    iconUrl: 'img/mrt.png',
+    iconSize: [45,45],
+    iconAnchor:[23,45],
+    popupAnchor: [0,0]
+})
+const gymIcon = L.icon({
+    iconUrl: 'img/gym.png',
+    iconSize: [45,45],
+    iconAnchor:[23,45],
+    popupAnchor: [0,0]
+})
+
+const barIcon = L.icon({
+    iconUrl: 'img/bar.png',
+    iconSize: [45,45],
+    iconAnchor:[23,45],
+    popupAnchor: [0,0]
+})
+
 async function main() {
 
     MRTLayer.clearLayers();
     restaurantLayer.clearLayers();
     gymLayer.clearLayers();
     barLayer.clearLayers();
+    searchRadiusLayer.clearLayers();
 
-    console.log('search point=============');
+    // console.log('search point=============');
     console.log(foursquare.centerpoint);
 
     //MRT
@@ -78,16 +121,16 @@ async function main() {
         "params": {
             "ll": foursquare.centerpoint[0] + ',' + foursquare.centerpoint[1],
             "categories": foursquare.categories.mrt,
-            "radius": 30000,
-            "limit": 5,
+            "radius": 40000,
+            "limit": 50,
             "sort": "distance"
         },
     });
     console.log("this is data ", mrtResponse)
 
     for (let MRT of mrtResponse.data.results) {
-        let marker = L.marker([MRT.geocodes.main.latitude, MRT.geocodes.main.longitude]);
-        marker.bindPopup(`<h1>${MRT.name}</h1>`);
+        let marker = L.marker([MRT.geocodes.main.latitude, MRT.geocodes.main.longitude],{icon:mrtIcon});
+        marker.bindPopup(`<h1>${MRT.name}</h1>`); 
         //maps.
         marker.addTo(MRTLayer);
     }
@@ -101,22 +144,19 @@ async function main() {
         "params": {
             "ll": foursquare.centerpoint[0] + ',' + foursquare.centerpoint[1],
             "catagories": foursquare.categories.restaurant,
-            "radius": 30000,
-            "limit": 5,
+            "radius": 40000,
+            "limit": 50,
             "sort": "distance"
         }
     });
 
     for (let restaurant of restaurantResponse.data.results) {
-        let circle = L.circle([restaurant.geocodes.main.latitude, restaurant.geocodes.main.longitude], {
-            color: "red",
-            fillColor: "red",
-            radius: 200,
-            fillOpacity: 0.5
-        });
-        circle.bindPopup(`<h1>${restaurant.name}</h1>`)
-        circle.addTo(restaurantLayer);
+        let restaurantMarker = L.marker([restaurant.geocodes.main.latitude, restaurant.geocodes.main.longitude],{icon: restaurantIcon});
+           
+        restaurantMarker.bindPopup(`<h1>${restaurant.name}</h1>`)
+        restaurantMarker.addTo(restaurantLayer);
     }
+    
 
     //gym
     let gymResponse = await axios.get(foursquare.URL, {
@@ -127,21 +167,17 @@ async function main() {
         "params": {
             "ll": foursquare.centerpoint[0] + ',' + foursquare.centerpoint[1],
             "categories": foursquare.categories.gym,
-            "radius": 30000,
-            "limit": 5,
+            "radius": 40000,
+            "limit": 50,
             "sort": "distance"
         }
     });
 
     for (let gym of gymResponse.data.results) {
-        let marker = L.circle([gym.geocodes.main.latitude, gym.geocodes.main.longitude], {
-            color: "green",
-            fillColor: "green",
-            radius: 200,
-            fillOpacity: 0.5
-        });
-        marker.bindPopup(`<h1>${gym.name}</h1>`);
-        marker.addTo(gymLayer);
+        let gymMarker = L.marker([gym.geocodes.main.latitude, gym.geocodes.main.longitude], {icon:gymIcon});
+           
+        gymMarker.bindPopup(`<h1>${gym.name}</h1>`);
+        gymMarker.addTo(gymLayer);
     }
 
     //bar
@@ -153,28 +189,24 @@ async function main() {
         "params": {
             "ll": foursquare.centerpoint[0] + ',' + foursquare.centerpoint[1],
             "categories": foursquare.categories.bar,
-            "radius": 30000,
-            "limit": 5,
+            "radius": 40000,
+            "limit": 50,
             "sort": "distance"
         }
     });
 
 
     for (let bar of barResponse.data.results) {
-        let marker = L.circle([bar.geocodes.main.latitude, bar.geocodes.main.longitude], {
-            color: "blue",
-            fillColor: "blue",
-            radius: 200,
-            fillOpacity: 0.5
-        });
-        marker.bindPopup(`<h1>${bar.name}</h1>`);
-        marker.addTo(barLayer);
+        let barMarker = L.marker([bar.geocodes.main.latitude, bar.geocodes.main.longitude], {icon:barIcon});
+       
+        barMarker.bindPopup(`<h1>${bar.name}</h1>`);
+        barMarker.addTo(barLayer);
     }
 }
 
 document.querySelector("#btnSearch").addEventListener("click", async function () {
     let searchValue = document.querySelector('#searchValue').value;
-    console.log(searchValue)
+    console.log(searchValue);
 
     // search mrt result
     let searchResponse = await axios.get(foursquare.URL, {
@@ -184,7 +216,7 @@ document.querySelector("#btnSearch").addEventListener("click", async function ()
         },
         "params": {
             "ll": singapore[0] + ',' + singapore[1],
-            "categories": foursquare.categories.mrt,
+            // "categories": foursquare.categories.mrt,
             "radius": 30000,
             "limit": 1,
             "query": searchValue
@@ -195,10 +227,95 @@ document.querySelector("#btnSearch").addEventListener("click", async function ()
     
 
     let searchPoint = [searchResponse.data.results[0].geocodes.main.latitude, searchResponse.data.results[0].geocodes.main.longitude]
+    
+    // call bar, restaurant, gym apis using searchpoint
+    //restaurant
+    let restaurantRequest = axios.get(foursquare.URL, {
+        "headers": {
+            "Accept": "application/json",
+            "Authorization": foursquare.API_KEY
+        },
+        "params": {
+            "ll": searchPoint.join(','),
+            "catagories": foursquare.categories.restaurant,
+            "radius": 1000, // can allow user to choose how big the search radius around selected MRT
+            "limit": 10,
+            "sort": "distance"
+        }
+    });
+
+    //bar
+    let barRequest = axios.get(foursquare.URL, {
+        "headers": {
+            "Accept": "application/json",
+            "Authorization": foursquare.API_KEY
+        },
+        "params": {
+            "ll": searchPoint.join(','),
+            "categories": foursquare.categories.bar,
+            "radius": 1000,
+            "limit": 10,
+            "sort": "distance"
+        }
+    });
+
+    //gym
+    let gymRequest = axios.get(foursquare.URL, {
+        "headers": {
+            "Accept": "application/json",
+            "Authorization": foursquare.API_KEY
+        },
+        "params": {
+            "ll": searchPoint.join(','),
+            "categories": foursquare.categories.gym,
+            "radius": 1000,
+            "limit": 10,
+            "sort": "distance"
+        }
+    });
+
+    let restaurantResponse = await restaurantRequest;
+    let barResponse = await barRequest;
+    let gymResponse =  await gymRequest;
+
+    for (let restaurant of restaurantResponse.data.results) {
+        let restaurantMarker = L.marker([restaurant.geocodes.main.latitude, restaurant.geocodes.main.longitude],{icon: restaurantIcon});
+           
+        restaurantMarker.bindPopup(`<h1>${restaurant.name}</h1>`)
+        restaurantMarker.addTo(restaurantLayer);
+    }
+
+    for (let bar of barResponse.data.results) {
+        let barMarker = L.marker([bar.geocodes.main.latitude, bar.geocodes.main.longitude], {icon:barIcon});
+       
+        barMarker.bindPopup(`<h1>${bar.name}</h1>`);
+        barMarker.addTo(barLayer);
+    }
+
+    // let testLayer = L.layerGroup().addTo(map);
+    for (let gym of gymResponse.data.results) {
+        let gymMarker = L.marker([gym.geocodes.main.latitude, gym.geocodes.main.longitude], {icon:gymIcon});
+
+        gymMarker.bindPopup(`<h1>${gym.name}</h1>`);
+        gymMarker.addTo(gymLayer);
+        // gymMarker.addTo(searchGymLayer);
+    }
+    
+    // Display search radius
+    L.circle(searchPoint, {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 1000 // change according to user selected radius
+    }).addTo(searchRadiusLayer);
+
     console.log(searchPoint);
+    console.log(barResponse.data.results);
+    console.log(restaurantResponse.data.results);
+    console.log(gymResponse.data.results);
     map.flyTo(searchPoint, 17);
     foursquare.centerpoint = searchPoint;
-    main();
+    // main();
 });
 
 main();
